bea0d2a39f4c403a33d5fce53f4f42f7096f486d
Mrnewliu <Mrnewliu@gmail.com>
2019-01-25 10:33:30 +0800

    first commit

---
+++ who1.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<utmp.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<time.h>
+
+
+
+int main()
+{
+	struct utmp current_record;
+	int utmpfd;
+	int reclen = sizeof(current_record);
+	
+	if( (utmpfd = open(UTMP_FILE, O_RDONLY)) == -1 )
+	{
+		perror( UTMP_FILE );
+		exit(1);
+	}
+	
+	while( read( utmpfd, &current_record,reclen) == reclen )
+	{
+		show_info( &current_record);	
+		
+
+	}
+	close(utmpfd);
+
+	return 0;
+}
+
+void show_info(struct utmp * utbufp)
+{
+	if( utbufp->ut_type != USER_PROCESS)
+		return;
+	printf("% -8.8s", utbufp->ut_name);
+	printf(" ");
+	printf("% -8.8s", utbufp->ut_line);	
+	printf(" ");
+	//printf("%10ld", utbufp->ut_time);
+	showtime(utbufp->ut_time);
+	printf(" ");
+	printf("\n");
+
+}
+
+void showtime(long timeval)
+{
+	char* cp ;
+	cp = ctime( &timeval);
+	printf("% 12.12s",cp+4);
+		
+	
+}
+
+
+
+
+
+
